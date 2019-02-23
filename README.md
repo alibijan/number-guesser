ba23dd7b88df088b679a51bb1c272d7e631e90a1
ali bijan <alibijan.98@gmail.com>
2019-02-22 20:34:31 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # number-guesser
-Number Guessing game
+<!-- Accessible on: https://alibijan.github.io/task-list/ -->
+
+Simple number guessing game.
+- This code is following Brad Traversy's course,
+> Modern JavaScript From The Beginning
---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+GAME FUNCTION:
+- Player must guess a number between 1-10
+- Player gets a certain # of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let player choose to play again
+*/
